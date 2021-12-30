0871c25ac6bb3dcdc3a4f5a69f98b2be1e72b205
gitoputrow <gitoputrowardana@gmail.com>
2021-12-30 23:15:21 +0700

    test

--- app/src/main/java/com/example/workout_v2/Login.kt
+++ app/src/main/java/com/example/workout_v2/Login.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -17,6 +18,32 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val database = FirebaseDatabase.getInstance().getReference()
+        val firebaseauth = FirebaseAuth.getInstance()
+        val firebaseAuthuserLogin = firebaseauth.currentUser
+        if (firebaseAuthuserLogin != null){
+            val email = firebaseAuthuserLogin.email
+            database.addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (childmain in snapshot.children){
+                        var childdata = childmain.key.toString()
+                        var emailUser = snapshot.child(childdata).child("email").value.toString()
+                        var username = snapshot.child(childdata).child("username").value.toString()
+                        if (email.toString().equals(emailUser)){
+                            val pindah = Intent(this@Login,MainActivity::class.java)
+                            pindah.putExtra("username",username)
+                            pindah.putExtra("current","workout")
+                            finish()
+                            startActivity(pindah)
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
         findViewById<TextView>(R.id.textView_regist).setOnClickListener {
             startActivity(Intent(this,register::class.java))
         }
@@ -33,11 +60,17 @@ class Login : AppCompatActivity() {
                     override fun onDataChange(snapshot: DataSnapshot) {
                        if (snapshot.hasChild("data$username")){
                            if (snapshot.child("data$username").child("password").value.toString().equals(passwword)){
-                               val pindah = Intent(this@Login,MainActivity::class.java)
-                               pindah.putExtra("username",username)
-                               pindah.putExtra("current","workout")
-                               finish()
-                               startActivity(pindah)
+                               val email = snapshot.child("data$username").child("email").value.toString()
+                               firebaseauth.signInWithEmailAndPassword(email,passwword)
+                                   .addOnSuccessListener {
+                                       val firebaseUser = firebaseauth.currentUser
+                                       val email = firebaseUser!!.email
+                                       val pindah = Intent(this@Login,MainActivity::class.java)
+                                       pindah.putExtra("username",username)
+                                       pindah.putExtra("current","workout")
+                                       finish()
+                                       startActivity(pindah)
+                                   }
                            }
                            else{
                                Toast.makeText(baseContext,"Wrong Password",Toast.LENGTH_SHORT).show()
